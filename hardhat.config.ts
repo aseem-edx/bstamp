43dc2afdb729072a59d0f5b0a37ad0b4708646f9
Aseem PM <a.palumannil@edexa.team>
2024-04-22 14:58:56 +0530

    configured the rpc network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,16 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "dotenv/config";
 
+const { NETWORK_URL, ACCOUNT_PRIVATE } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    rpc: {
+      accounts: [ACCOUNT_PRIVATE!],
+      url: NETWORK_URL,
+    },
+  },
 };
 
 export default config;

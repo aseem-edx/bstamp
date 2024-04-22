19189828d5edfa287c1e14ee25467ecf71574a16
Aseem PM <a.palumannil@edexa.team>
2024-04-22 13:17:42 +0530

    added upgradable deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,11 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
+  const BStamp = await ethers.getContractFactory("BSTAMP");
+  const bStamp = await upgrades.deployProxy(BStamp);
+  await bStamp.waitForDeployment();
 
-
+  console.log("BStamp Address:", bStamp.target);
 }
 
 main().catch((error) => {
